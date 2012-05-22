d196bbd32da96109982e77a07fa44eec08d49c63
Zhehao Mao <zhehao.mao@gmail.com>
2012-05-22 10:46:05 -0400

    starting to implement activity indication on keys

--- drawing.c
+++ drawing.c
@@ -1,5 +1,29 @@
 #include "drawing.h"
 
+int note_type(int note){
+	if(note < 4 && note % 2 == 1)
+		return BLACK_NOTE;
+	if(note > 5 && note < 11 && note % 2 == 0)
+		return BLACK_NOTE;
+	return WHITE_NOTE;
+}
+
+int white_note(int note){
+	if(note < 5){
+		return note / 2;
+	} else if(note < 12){
+		return (note-5)/2 + 4;
+	}
+	return 7;
+}
+
+int black_note(int note){
+	if(note < 4){
+		return (note-1)/2;
+	} 
+	return note/2 - 1;
+}
+
 void draw_piano(Display *display, Window window, GC gc, int width, int height){
 	long white = WhitePixel(display, DefaultScreen(display));
 	long black = BlackPixel(display, DefaultScreen(display));
--- drawing.h
+++ drawing.h
@@ -3,6 +3,14 @@
 
 #include <X11/Xlib.h>
 
+enum {
+    WHITE_NOTE,
+    BLACK_NOTE
+};
+
 void draw_piano(Display *display, Window window, GC gc, int width, int height);
+int note_type(int note);
+int white_note(int note);
+int black_note(int note);
 
 #endif
